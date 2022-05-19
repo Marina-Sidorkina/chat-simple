4150be5041115306e55381186ffddf0be80df2b6
Marina Sidorkina <m.sidorkina55@gmail.com>
2022-05-20 00:51:14 +0300

    Add fixes.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { Server } = require('socket.io');
+const http = require('http');
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const server = http.createServer(app);
+const io = new Server(server);
+
 
 const rooms = new Map();
 
