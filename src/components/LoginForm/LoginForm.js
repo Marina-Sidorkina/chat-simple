d6150ae0d2b0bae2720b22ac293bd3e27f7f7a37
Marina Sidorkina <m.sidorkina55@gmail.com>
2022-05-23 23:50:31 +0300

    Add state to LoginForm.

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginForm.css';
 import socket from '../../socket';
 
 const LoginForm = () => {
+  const [ roomId, setRoomId ] = useState('');
+  const [ userName, setUserName ] = useState('');
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(roomId, userName);
+  }
+
   return (
-    <form className="login-form">
-      <input className="login-form__input form-control form-control-lg" type="text" placeholder="Room ID"
-             aria-label="Room ID" />
-      <input className="login-form__input form-control form-control-lg" type="text" placeholder="Ваше имя"
-             aria-label="Ваше имя" />
-      <button className="login-form__btn btn btn-success" type="submit">Войти</button>
+    <form className="login-form"
+          onSubmit={onSubmit}>
+      <input className="login-form__input form-control form-control-lg"
+             type="text"
+             placeholder="Room ID"
+             aria-label="Room ID"
+             required
+             value={roomId}
+             onChange={(evt) => setRoomId(evt.target.value)}
+      />
+      <input className="login-form__input form-control form-control-lg"
+             type="text"
+             placeholder="Ваше имя"
+             aria-label="Ваше имя"
+             required
+             value={userName}
+             onChange={(evt) => setUserName(evt.target.value)}
+      />
+      <button className="login-form__btn btn btn-success"
+              type="submit">Войти</button>
     </form>
   );
 }
